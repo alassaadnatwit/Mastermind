1d9aeae88055eed5c26675569f7f66523252ce53
fotogatwit <71099972+fotogatwit@users.noreply.github.com>
2021-11-22 12:24:21 -0500

    Add files via upload

---
+++ MasterMind/src/masterMind/Main.java
@@ -0,0 +1,29 @@
+package masterMind;
+
+import javafx.application.Application;
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.DoubleProperty;
+import javafx.event.EventType;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.awt.*;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("MasterMind.fxml"));
+        primaryStage.setResizable(false);
+        primaryStage.setScene(new Scene(root, 380.0, 706.0));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
+
